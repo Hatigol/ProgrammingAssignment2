a00f16a72a3f5182525dbfaed998f578bf99c87d
Hatigol <sba999@hotmail.com>
2015-03-21 23:17:52 -0700

    ProgrammingAssignment2
    Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ## initialize local variable
+  inv <- NULL
+  
+  ##1 of 4 function definition. Sets the cached matrix value
+  set <- function(y) {
+    x <<- y ##  assign value to a variable in the parent environment.
+    inv  <<- NULL
+  }
+  ##2 of 4 function definition; returns the matrix
+  get <- function() x
+  
+  ##3 of 4 function definition. sets the cached inverse
+  setinv <- function(inverse) inv <<- inverse
+  
+  ##4 of 4 function definition, returns the cached inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
@@ -12,4 +31,24 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## sets the local variable to the inverse
+  inv <- x$getinv() 
+  
+  ## if inv is not null, it will get cached data
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  ## if inv is null, the function will obtain the matrix from the makeCacheMatrix object and assign it to data
+  data <- x$get()
+  
+  ## get the inverse
+  inv <- solve(data, ...)
+  
+  ## set the inverse of matrix
+  x$setinv(inv)
+  
+  ## returns the inverse
+  inv
 }
